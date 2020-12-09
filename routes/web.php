847276f1e36907b587892885894767465255025f
a.ishihara <a.ishihara@lexues.co.jp>
2020-12-09 16:13:43 +0900

    scssの勉強

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//このメソッドの内部で認証系に必要なルーティング定義を一通り追加してる
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/sass', function () {
+    return view('sass');
+});
